b4611827b71030ac3b03ab8124c55f654e8f0ca9
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2021-12-24 20:42:00 +0700

    initial-commmit

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,59 @@
+import { Card, Button } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+// import API from '../context/API/data.json'
+
+const SingleProduct = ({ prod }) => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
+  return (
+    <div className="products">
+      <Card>
+        {/* <Card.Img variant="top" src={prod.image} alt={prod.name} /> */}
+        <Card.Img variant="top" src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80" alt={prod.name} />
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>Rp.{prod.price}</span>
+            {prod.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 days delivery</div>
+            )}
+            <Rating rating={prod.ratings} />
+          </Card.Subtitle>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              variant="danger"
+              onClick={() =>
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                })
+              }
+            >
+              Remove from Cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() =>
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                })
+              }
+              disabled={!prod.inStock}
+            >
+              {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default SingleProduct;
